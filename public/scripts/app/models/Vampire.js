994b8abcb66911aa1793da7a3e7c7912c6b56e47
Andrew Sayman <andrew.sayman@gmail.com>
2015-07-23 23:10:47 -0400

    Add method for calculating total costs of a trait

--- public/scripts/app/models/Vampire.js
+++ public/scripts/app/models/Vampire.js
@@ -148,7 +148,7 @@ define([
                 "backgrounds_1_remaining": 1,
                 "disciplines_2_remaining": 1,
                 "disciplines_1_remaining": 1,
-                "initial_xp": false
+                "initial_xp": 30
             });
             return creation.save().then(function (newCreation) {
                 self.set("creation", newCreation);
@@ -158,6 +158,121 @@ define([
 
         is_being_created: function() {
             return !this.get("creation").get("completed");
+        },
+
+        discipline_is_in_clan: function(trait) {
+            // TODO real implementation
+            return _.sample([true, false]);
+        },
+
+        get_cost_table: function(cost_per_entry) {
+            return _.map(_.range(1, 10), function(i) {
+                return i * cost_per_entry;
+            });
+        },
+
+        get_cost_on_table: function(ct, value) {
+            return _(ct).take(value).sum().value();
+        },
+
+        get_trait_cost_on_table: function(ct, trait) {
+            var value = trait.get("value");
+            var free_value = trait.get("free_value") || 0;
+            var total_cost = self.get_cost_on_table(ct, value);
+            var free_cost = self.get_cost_on_table(ct, value);
+            return total_cost - free_cost;
+        },
+
+        calculate_total_cost: function(trait) {
+            var self = this;
+            var category = trait.get("category");
+            var value = trait.get("value");
+            var free_value = trait.get("free_value") || 0;
+            var mod_value = value - free_value;
+
+            if ("attributes" == category) {
+                return mod_value * 3;
+            }
+
+            if ("disciplines" == category && self.discipline_is_in_clan(trait)) {
+                return self.get_trait_cost_on_table(self.get_cost_table(3), trait);
+            }
+
+            if ("humanity" == category) {
+                return mod_value * 10;
+            }
+
+            /* Merits can have a "free" value if they're given by some other merit */
+            if ("merits" == category) {
+                return mod_value;
+            }
+
+            if ("rituals" == category) {
+                return mod_value * 2;
+            }
+
+            var generation = 0;
+            _.each(self.get("backgrounds"), function(b) {
+                if (b.get("name") == "Generation") {
+                    generation = b.get("value");
+                }
+            });
+
+            var background_ct, skill_ct, ooc_discipline_ct, technique_cost, ic_elder_cost, ooc_elder_cost;
+
+            if ("backgrounds" == category) {
+                if (generation == 1) {
+                    background_ct = self.get_cost_table(1);
+                } else {
+                    background_ct = self.get_cost_table(2);
+                }
+                return self.get_trait_cost_on_table(background_ct, trait);
+            }
+
+            if ("skills" == category) {
+                if (generation == 1) {
+                    skill_ct = self.get_cost_table(1);
+                } else {
+                    skill_ct = self.get_cost_table(2);
+                }
+                return self.get_trait_cost_on_table(skill_ct, trait);
+            }
+
+            if ("disciplines" == category) {
+                // Must be OOC to have gotten this far
+                if (generation < 5) {
+                    ooc_discipline_ct = self.get_cost_table(4);
+                } else {
+                    ooc_discipline_ct = self.get_cost_table(5);
+                }
+                return self.get_trait_cost_on_table(ooc_discipline_ct, trait);
+            }
+
+            if ("techniques" == category) {
+                if (generation < 3) {
+                    technique_cost = 12;
+                } else if (generation == 3) {
+                    technique_cost = 20;
+                }
+                return mod_value * technique_cost;
+            }
+
+            if ("elder_disciplines" == category) {
+                if (generation >= 3) {
+                    ic_elder_cost = 18;
+                }
+                if (generation == 5) {
+                    ooc_elder_cost = 30;
+                } else if (generation >= 3) {
+                    ooc_elder_cost = 30;
+                }
+                if (self.discipline_is_in_clan(trait)) {
+                    return mod_value * ic_elder_cost;
+                } else {
+                    return mod_value * ooc_elder_cost;
+                }
+            }
+
         }
 
     } );
