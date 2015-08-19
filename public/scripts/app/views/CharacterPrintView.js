e8501ca0fa009f4f5f96159bd1fe37dd73520e5e
Andrew Sayman <andrew.sayman@gmail.com>
2015-08-18 23:29:26 -0400

    More work on the print template

--- public/scripts/app/views/CharacterPrintView.js
+++ public/scripts/app/views/CharacterPrintView.js
@@ -12,30 +12,45 @@ define([
 
         // The View Constructor
         initialize: function() {
-
+            var self = this;
+            self.base_abilities = [];
+            self.base_abilities.push(["Academics", "Animal Ken", "Athletics", "Awareness", "Brawl", "Computer", "Crafts", "Dodge", "Drive"]);
+            self.base_abilities.push(["Empathy", "Firearms", "Intimidation", "Investigation", "Leadership", "Linguistics", "Lore", "Medicine", "Melee", "Occult"]);
+            self.base_abilities.push(["Performance", "Science", "Security", "Stealth", "Streetwise", "Subterfuge", "Survival"]);
+            self.base_abilities = _.flatten(self.base_abilities);
         },
 
         // Renders all of the Category models on the UI
         render: function() {
 
             var character = this.model;
-            var cols = [];
-            cols.push(["Academics", "Animal Ken", "Athletics", "Awareness", "Brawl", "Computer", "Crafts", "Dodge", "Drive"]);
-            cols.push(["Empathy", "Firearms", "Intimidation", "Investigation", "Leadership", "Linguistics", "Lore", "Medicine", "Melee", "Occult"]);
-            cols.push(["Performance", "Science", "Security", "Stealth", "Streetwise", "Subterfuge", "Survival"]);
-            var a = _.zip.apply(_, cols)
-            var s = character.get("skills");
-            var f = _.find(s, {name: "Athletics"});
-            var r = _.result(f, 'name', 0);
-            skillsLookup = _.groupBy(s, function(skill) {
-                return skill.get("name");
+            var columnCount = 3;
+            var sortedSkills = character.get("skills");
+            sortedSkills = _.sortBy(sortedSkills, "attributes.name");
+            sortedSkills = _.map(sortedSkills, function (skill) {
+                var name = skill.get("name");
+                if (-1 == name.indexOf(":")) {
+                    return name + " x" + skill.get("value");
+                } else {
+                    var rootName = name.slice(0, name.indexOf(':'));
+                    var rightName = name.slice(name.indexOf(':'));
+                    return rootName + " x" + skill.get("value") + rightName;
+                }
+            })
+            var groupedSkills = {0: [], 1: [], 2: []};
+            var shiftAmount = _.ceil(sortedSkills.length / columnCount);
+            _.each(_.range(columnCount), function (i) {
+                groupedSkills[i] = _.take(sortedSkills, shiftAmount);
+                sortedSkills = _.drop(sortedSkills, shiftAmount);
             });
+            groupedSkills = _.zip(groupedSkills[0], groupedSkills[1], groupedSkills[2]);
 
             // Sets the view's template property
             this.template = _.template(
                 $( "script#characterPrintView" ).html())(
                 { "character": this.model,
-                  "skills": skillsLookup} );
+                  "skills": sortedSkills,
+                  "groupedSkills": groupedSkills} );
 
             // Renders the view's template inside of the current listview element
             this.$el.find("div[role='main']").html(this.template);
