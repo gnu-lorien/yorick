1450b34814abff007e0c13f796642a258d1a44fb
Andrew Sayman <andrew.sayman@gmail.com>
2016-01-30 17:26:08 -0500

    Add a new troupe

--- public/scripts/app/models/Troupe.js
+++ public/scripts/app/models/Troupe.js
@@ -6,6 +6,15 @@ define([
 
     // The Model constructor
     var Model = Parse.Object.extend( "Troupe", {
+        initialize: function(options) {
+            var self = this;
+            var acl = new Parse.ACL();
+            acl.setPublicReadAccess(true);
+            acl.setPublicWriteAccess(false);
+            acl.setRoleReadAccess("Administrator", true);
+            acl.setRoleWriteAccess("Administrator", true);
+            self.setACL(acl);
+        }
 
     } );
 
