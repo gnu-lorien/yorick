4c43b71fce5713bd8227696810d7352b45e27a42
Andrew Sayman <andrew.sayman@gmail.com>
2016-02-10 01:49:06 -0500

    Visually divide fail logging for multiple failures

--- public/scripts/app/helpers/PromiseFailReport.js
+++ public/scripts/app/helpers/PromiseFailReport.js
@@ -5,20 +5,26 @@ define([
 
     var process_single = function (error) {
         if (_.has(error, "message")) {
-            console.log("Error in promise " + error.message);
+            console.log("Error in promise:" + error.message);
             if (Parse.Error.INVALID_LINKED_SESSION == error.code) {
                 Parse.User.logOut();
             }
         } else {
-            console.log("Error in promise " + error);
+            console.log("Error in promise:" + error);
         }
     };
 
     var func = function (error) {
         if (_.isArray(error)) {
-            _.each(error, function (e) {
-                process_single(e);
-            })
+            console.log("Error in a multi-promise follows {");
+            _.each(error, function (e, i) {
+                if (_.has(e, "success")) {
+                    console.log("" + i + " succeeded at " + e.updatedAt);
+                } else {
+                    process_single(e);
+                }
+            });
+            console.log("}");
         } else {
             process_single(error);
         }
