e63957ce4da86096d66511c946683c15cf5b988a
Andrew Sayman <andrew.sayman@gmail.com>
2016-02-11 03:04:09 +0000

    Changed serverjs?

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
-var express = require('express')
-var serveStatic = require('serve-static')
+var express = require('express'),
+    serveStatic = require('serve-static'),
+    fs = require('fs'),
+    privateKey = fs.readFileSync('key.pem'),
+    certificate = fs.readFileSync('cert.pem'),
+    http = require('http'),
+    https = require('https');
 
+var credentials = {key: privateKey, cert: certificate};
 var app = express()
 
 app.use(serveStatic('public'));
-app.listen(8000, '0.0.0.0');
+
+http.createServer(app).listen(8000, '0.0.0.0');
+//https.createServer(credentials, app).listen(8000, '0.0.0.0');
