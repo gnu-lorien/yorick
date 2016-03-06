194b420f987e455fb159e824e13e1e989c66dac0
Andrew Sayman <andrew.sayman@gmail.com>
2016-03-05 19:56:08 -0500

    Popup error messages for the user to see

--- public/scripts/app/loadall.js
+++ public/scripts/app/loadall.js
@@ -19,6 +19,30 @@ require([
         this.BNSMETV1_ClanRules = new ClanRules;
         this.BNSMETV1_ClanRules.fetch();
 
-        nprogress.done();
+        trackJs.configure({
+            onError: function (payload) {
+                _.defer(function () {
+                    var closebtn = '<a href="#" data-rel="back" class="ui-btn ui-corner-all ui-btn-a ui-icon-delete ui-btn-icon-notext ui-btn-right">Close</a>',
+                        header = '<div data-role="header"><h2>Error Reported</h2></div>',
+                        span = '<span>' + payload.message + '</span>',
+                        popup = '<div data-role="popup" id="popup-global-error" data-short="global-error" data-theme="none" data-overlay-theme="a" data-corners="false" data-tolerance="15"></div>';
+                    $(header)
+                        .appendTo($(popup)
+                            .appendTo($.mobile.activePage)
+                            .popup())
+                        .toolbar()
+                        .before(closebtn)
+                        .after(span);
+                    var fallback = _.delay(function () {
+                        $("#popup-global-error").popup("open");
+                    }, 2000);
+                    $("#popup-global-error").load(function () {
+                        $("#popup-global-error").popup("open");
+                        clearTimeout(fallback);
+                    });
+                });
+                return true;
+            }
+        })
     });
 });
