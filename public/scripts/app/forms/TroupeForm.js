629383c2917f0d3c258a4e307e63485be8a09d5b
Andrew Sayman <andrew.sayman@gmail.com>
2016-03-08 19:41:52 -0500

    Add location to troupe values

--- public/scripts/app/forms/TroupeForm.js
+++ public/scripts/app/forms/TroupeForm.js
@@ -9,6 +9,7 @@ define([
             {name: "name", label: "Name", control: "input"},
             {name: "shortname", label: "Short Name", control: "input"},
             {name: "shortdescription", label: "Short Public Description", control: "input"},
+            {name: "location", label: "Location", control: "input"},
             {control: "spacer"},
             {name: "description", label: "Long Public Description", control: "textarea"},
             //{control: "button", label: "Add New"},
