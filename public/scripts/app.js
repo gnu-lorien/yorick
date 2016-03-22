04584aa090e7954b1aca366dc435db52385a251e
Andrew Sayman <andrew.sayman@gmail.com>
2016-03-21 21:27:23 -0400

    Improve load times through minifying files

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -9,15 +9,15 @@ requirejs.config( {
 
 		// Core Libraries
 		jquery: "//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery",
-		jquerymobile: "jquery.mobile-1.4.5",
+		jquerymobile: "jquery.mobile-1.4.5.min",
 		jscookie: "js.cookie",
 		underscore: "//cdn.jsdelivr.net/lodash/3.10.0/lodash",
 		backbone: "//cdnjs.cloudflare.com/ajax/libs/backbone.js/1.1.2/backbone",
 		//parse: "//www.parsecdn.com/js/parse-1.5.0.min",
-		parse: "parse-1.5.0",
+		parse: "parse-1.5.0.min",
 		pretty: "prettyprint",
 		moment: "moment",
-		vis: "vis",
+		vis: "vis.min",
 		backform: "backform",
 		nprogress: "nprogress",
 
