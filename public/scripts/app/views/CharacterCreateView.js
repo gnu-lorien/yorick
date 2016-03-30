0726f51ecc17a107e62f0c737a6d7ebc794cdefb
Andrew Sayman <andrew.sayman@gmail.com>
2016-03-29 23:27:13 -0400

    Move character creation to a template

--- public/scripts/app/views/CharacterCreateView.js
+++ public/scripts/app/views/CharacterCreateView.js
@@ -4,8 +4,9 @@
 // Includes file dependencies
 define([
 	"jquery",
-	"backbone"
-], function( $, Backbone) {
+	"backbone",
+    "text!../templates/character-create-view.html"
+], function( $, Backbone, character_create_view_html ) {
 
     // Extends Backbone.View
     var View = Backbone.View.extend( {
@@ -27,7 +28,7 @@ define([
         render: function() {
 
             // Sets the view's template property
-            this.template = _.template( $( "script#characterCreateView" ).html())({ "character": this.model } );
+            this.template = _.template(character_create_view_html)({ "character": this.model } );
 
             // Renders the view's template inside of the current listview element
             this.$el.find("div[role='main']").html(this.template);
