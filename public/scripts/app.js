e8fdb6a6772c37becb1a8170ff057fc9288cd081
Andrew Sayman <andrew.sayman@gmail.com>
2016-04-02 00:27:57 -0400

    Update bust

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,7 +2,7 @@
 requirejs.config( {
 	baseUrl: "scripts/lib",
 	//urlArgs: "bust=" + (new Date()).getTime(),
-	urlArgs: "bust=" + 18,
+	urlArgs: "bust=" + 19,
 
 	// 3rd party script alias names
 	paths: {
