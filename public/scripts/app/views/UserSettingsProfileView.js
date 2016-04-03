74eb381c601e1f6ee907f9b6e03c9dd99929f061
Andrew Sayman <andrew.sayman@gmail.com>
2016-04-02 22:19:33 -0400

    Migrate user settings to backform
    This will make it easier to add features to the user account page going
    forward

--- public/scripts/app/views/UserSettingsProfileView.js
+++ public/scripts/app/views/UserSettingsProfileView.js
@@ -2,51 +2,67 @@
 define([
     "jquery",
     "backbone",
-    "parse"
-], function ($, Backbone, Parse) {
+    "parse",
+    "backform"
+], function ($, Backbone, Parse, Backform) {
 
     // Extends Backbone.View
     var UserSettingsProfileView = Backbone.View.extend({
         initialize: function () {
-            _.bindAll(this, "allowUpdates");
-        },
+            var view = this;
+            view.errorModel = new Backbone.Model();
+            this.form = new Backform.Form({
+                errorModel: view.errorModel,
+                model: Parse.User.current() || new Backbone.Model,
+                fields: [
+                    {name: "realname", label: "Real Name", control: "input"},
+                    {name: "email", label: "Email", control: "input", type: "email"},
+                    {name: "submit", label: "Update", control: "button", disabled: true, id: "submit"}
+                ],
+                events: {
+                    "change": function (e) {
+                        e.preventDefault();
+                        this.$('button[name=submit]').removeAttr("disabled");
+                        var s = this.fields.get("submit");
+                        if ("success" == s.get("status")) {
+                            s.set({status: "", message: ""});
+                            this.$el.enhanceWithin();
+                        }
+                    },
+                    "submit": function (e) {
+                        var self = this;
+                        e.preventDefault();
+                        $.mobile.loading("show");
+                        self.undelegateEvents();
+                        self.model.errorModel.clear();
 
-        events: {
-            "submit form.profile-form": "update",
-            "change #input-name": "allowUpdates",
-            "change #input-email": "allowUpdates",
-            "click .update": "update",
-        },
+                        self.model.save().then(function () {
+                            self.fields.get("submit").set({status: "success", message: "Successfully Updated"});
+                            self.$el.enhanceWithin();
+                        }, function (error) {
+                            self.fields.get("submit").set({status: "error", message: _.escape(error.message)});
+                            self.$el.enhanceWithin();
+                        }).always(function () {
+                            $.mobile.loading("hide");
+                            self.delegateEvents();
+                        });
 
-        allowUpdates: function(e) {
-            this.$(".update").removeAttr("disabled");
-        },
-
-        update: function(e) {
-            var self = this;
-            var user = Parse.User.current();
-            user.set("realname", this.$("#input-name").val());
-            user.set("email", this.$("#input-email").val());
-            $.mobile.loading("show");
-            self.undelegateEvents();
-            user.save().then(function () {
-                self.$(".error").hide();
-                self.render();
-            }, function(error) {
-                self.$(".error").html(_.escape(error.message)).show();
-            }).always(function() {
-                $.mobile.loading("hide");
-                self.delegateEvents();
+                        return false;
+                    }
+                }
             });
-
-            return false;
         },
 
         render: function () {
-            this.template = _.template($("#userSettingsProfileView").html())({
-                "user": Parse.User.current(),
-            });
-            this.$el.find("div[role='main']").html(this.template);
+            var view = this;
+
+            if (this.form.model !== Parse.User.current()) {
+                var errorModel = this.form.errorModel;
+                this.form.model = Parse.User.current();
+                this.form.model.errorModel = errorModel;
+            }
+            this.form.setElement(this.$el.find("form.profile-form"));
+            this.form.render();
             this.$el.enhanceWithin();
         }
     });
