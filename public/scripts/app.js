6cb90c019862e2e53cdedca406ceabd0d7667b40
Andrew Sayman <andrew.sayman@gmail.com>
2016-04-23 13:47:11 -0400

    Force a bust

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,7 +2,7 @@
 requirejs.config( {
 	baseUrl: "scripts/lib",
 	//urlArgs: "bust=" + (new Date()).getTime(),
-	urlArgs: "bust=" + 20,
+	urlArgs: "bust=" + 21,
 
 	// 3rd party script alias names
 	paths: {
