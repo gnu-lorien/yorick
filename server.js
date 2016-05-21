96b1dc1d699842e377a086102e776c0c9a108bb4
Andrew Sayman <andrew.sayman@gmail.com>
2016-05-21 14:24:27 -0400

    Use environment to find the correct files to serve

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ var express = require('express'),
 
 var app = express()
 
-app.use(serveStatic('public'));
+app.use(serveStatic(process.env.PUBLIC_BASE));
 
 http.createServer(app).listen(8000, '0.0.0.0');
