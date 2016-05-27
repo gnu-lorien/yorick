71a21a2a5bbbcc1217a4bae4fc1113a624640cc6
Andrew Sayman <andrew.sayman@gmail.com>
2016-05-26 23:38:59 -0400

    Button to sign up through facebook too

--- public/scripts/app/views/SignupView.js
+++ public/scripts/app/views/SignupView.js
@@ -8,7 +8,8 @@ define([
     // Extends Backbone.View
     var SignupView = Backbone.View.extend( {
         events: {
-            "submit form.signup-form": "signUp"
+            "submit form.signup-form": "signUp",
+            "click #signup-with-facebook": "signUpWithFacebook"
         },
 
         el: "#signup",
@@ -18,10 +19,44 @@ define([
             this.render();
         },
 
+        signUpWithFacebook: function(e) {
+            var self = this;
+            e.preventDefault();
+            self.undelegateEvents();
+            self.$(".signup-form .error").hide();
+            this.$(".signup-form button").attr("disabled", "disabled");
+
+            Parse.FacebookUtils.logIn("email").then(function (user) {
+                hello('facebook').api('/me').then(function (r) {
+                    console.log(JSON.stringify(r));
+                }, function (error) {
+                    console.log(JSON.stringify(error));
+                });
+                if (!user.has("email") || !user.has("realname")) {
+                    console.log("Missing email or realname");
+                    user.set("email", user.get("authData").facebook.email);
+                    user.set("realname", user.get("authData").facebook.realname);
+                    return user.save();
+                }
+                return Parse.Promise.as([]);
+            }).then(function () {
+                location.reload();
+            }, function (error) {
+                console.log(JSON.stringify(error));
+                self.$(".signup-form .error").html(_.escape(error.message)).show();
+                self.$(".signup-form button").removeAttr("disabled");
+                self.delegateEvents();
+            });
+        },
+
         signUp: function(e) {
             var self = this;
             var username = this.$("#signup-username").val();
             var password = this.$("#signup-password").val();
+            e.preventDefault();
+            e.undelegateEvents();
+            self.$(".signup-form .error").hide();
+            this.$(".signup-form button").attr("disabled", "disabled");
 
             Parse.User.signUp(username, password, { }, {
                 success: function(user) {
@@ -32,10 +67,9 @@ define([
                 error: function(user, error) {
                     self.$(".signup-form .error").html(_.escape(error.message)).show();
                     self.$(".signup-form button").removeAttr("disabled");
+                    e.delegateEvents();
                 }
             });
-
-            this.$(".signup-form button").attr("disabled", "disabled");
 
             return false;
         },
