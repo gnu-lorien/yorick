bcc1d50c96a2707fa66f8120598f1fcbc9051f5e
Andrew Sayman <andrew.sayman@gmail.com>
2016-07-09 23:14:21 -0400

    Remove dumb logging stuff in initial radio channel

--- public/scripts/app/helpers/UserWreqr.js
+++ public/scripts/app/helpers/UserWreqr.js
@@ -8,11 +8,5 @@ define([
 
     var userChannel = Backbone.Wreqr.radio.channel('user');
 
-    Backbone.Wreqr.radio.reqres.setHandler("user", "testing", function(id) {
-        return "deez" + id;
-    });
-
-    console.log(userChannel.reqres.request('testing', 'anid'));
-    
     return userChannel;
 } );
