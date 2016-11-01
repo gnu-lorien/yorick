d1cf4b8286a9a65a61a02bd5a0b33651c82f9f1d
Andrew Sayman <andrew.sayman@gmail.com>
2016-11-01 02:54:56 +0000

    Remove timeouts when loading js files

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,6 +3,7 @@ requirejs.config( {
 	baseUrl: "scripts/lib",
 	//urlArgs: "bust=" + (new Date()).getTime(),
 	urlArgs: "bust=010101",
+	waitSeconds: 0,
 
 	// 3rd party script alias names
 	paths: {
