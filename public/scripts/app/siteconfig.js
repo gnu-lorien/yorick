0d124f87e4402425ed08f3a6069232bf24b20cd0
Andrew Sayman <andrew.sayman@gmail.com>
2016-11-12 20:28:12 +0000

    Set up netlify for custom staging domain

--- public/scripts/app/siteconfig.js
+++ public/scripts/app/siteconfig.js
@@ -3,9 +3,9 @@ define([
 ], function () {
 
     var ConfigPubstorm = {
-        serverURL: "https://api.undergroundtheater.org/parse",
+        serverURL: "https://stagingapi.undergroundtheater.org/parse",
         facebookAppId: "1606746326305984",
-        redirect_uri: "https://yorick.pubstorm.site/index.html"
+        redirect_uri: "https://stagingpatron.undergroundtheater.org/index.html"
     };
 
     var ConfigPatron = {
@@ -20,21 +20,19 @@ define([
         redirect_uri: "http://localhost/index.html"
     };
 
-    var ConfigGnuLorienDev = {
+    var ConfigOldGnuLorienDev = {
         serverURL: "http://localhost:1337/parse",
         facebookAppId: "1607159299598020",
         redirect_uri: "http://localhost:63342/yorick/public/index.html",
         SAMPLE_TROUPE_ID: "k7zf9B7bwV"
     };
 
-
-    var Config = {
+    var ConfigGnuLorienDev = {
         serverURL: "https://yorick-undergroundtheater-gnu-lorien.c9users.io/parse",
         redirect_uri: "https://yorick-undergroundtheater-gnu-lorien.c9users.io/index.html",
         SAMPLE_TROUPE_ID: "zCQcZnlFx5"
     };
     
-    return Config;
-    //return ConfigPatron;
+    return ConfigGnuLorienDev;
 
 });
