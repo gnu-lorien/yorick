7a2a97112524f1863d26f3cc38de092e40610312
Andrew Sayman <andrew.sayman@gmail.com>
2016-11-13 05:15:09 +0000

    Remove unused stuff from DescriptionsView

--- public/scripts/app/views/DescriptionsView.js
+++ public/scripts/app/views/DescriptionsView.js
@@ -13,147 +13,6 @@ define([
     "papaparse"
 ], function( _, $, Backbone, character_summarize_list_item_html, Marionette, Vampire, Backform, character_summarize_list_item_csv_html, character_summarize_list_item_csv_header_grouped_html, PromiseFailReport, Papa ) {
 
-    var PrettyView = Marionette.ItemView.extend({
-        tagName: "li",
-        className: "ul-li-has-thumb",
-        template: _.template(character_summarize_list_item_html),
-        initialize: function(options) {
-            this.mode = options.mode;
-            this.name = options.name;
-        },
-        
-        templateHelpers: function () {
-            var self = this;
-            return {
-                e: self.model,
-                mode: self.mode,
-                name: self.name
-            };
-        },
-        
-        events: {
-            "click": "clicked",
-        },
-
-        clicked: function(e) {
-            var self = this;
-            e.preventDefault();
-            $.mobile.loading("show");
-            var targete = $(e.currentTarget);
-            var pickedId = targete.attr("backendId");
-            var tmpl = _.template(self.click_url)({character_id: pickedId});
-            window.location.hash = tmpl;
-        },
-        
-        onRender: function () {
-            this.$el.enhanceWithin();
-        }
-    });
-    
-    var CSVView = Marionette.ItemView.extend({
-        tagName: "li",
-        template: _.template(character_summarize_list_item_csv_html),
-        initialize: function(options) {
-            this.mode = options.mode;
-            this.name = options.name;
-            this.columnNames = options.columnNames;
-        },
-        
-        templateHelpers: function () {
-            var self = this;
-            return {
-                e: self.model,
-                mode: self.mode,
-                name: self.name,
-            };
-        },
-
-    });
-    
-    var CSVHeaderGroupedView = Marionette.ItemView.extend({
-        tagName: "li",
-        template: _.template(character_summarize_list_item_csv_header_grouped_html),
-        initialize: function(options) {
-            this.mode = options.mode;
-            this.name = options.name;
-            this.columnNames = options.columnNames;
-        },
-        
-        templateHelpers: function () {
-            var self = this;
-            return {
-                e: self.model,
-                mode: self.mode,
-                name: self.name,
-                columnNames: self.columnNames
-            };
-        },
-
-    });
-    
-    var CharactersView = Marionette.CollectionView.extend( {
-        tagName: 'ul',
-        childView: PrettyView,
-        childViewOptions: function(model, index) {
-            var self = this;
-            return {
-                mode: self.mode,
-                name: self.name,
-                columnNames: self.columnNames
-            }
-        },
-        
-        onRender: function () {
-            var self = this;
-            /*
-            self.$el.filterable({
-                input: "#troupes-characters-filter"
-            });
-            self.$el.filterable("enable");
-            */
-            self.$el.attr('data-role', 'listview');
-            self.$el.attr('data-inset', 'true');
-            self.$el.attr('data-filter', 'true');
-            self.$el.attr('data-input', '#troupes-summarize-characters-filter');
-            /*
-            self.$el.enhanceWithin();
-            */
-        },
-        
-        /*
-        onAddChild: function() {
-            var self = this;
-            self.$el.filterable("refresh");
-        },
-        */
-        onDomRefresh: function() {
-            var self = this;
-            /*
-            self.$el.filterable("refresh");           
-            self.$el.filterable("enable");
-            */
-        }
-        
-    } );
-    
-    var FilterButton = Marionette.ItemView.extend({
-        className: "ui-block-b",
-        template: function (serialized_model) {
-            return _.template("<button><%= name %></button>")(serialized_model);
-        },
-        events: {
-            "click": "filterwith"
-        },
-        filterwith: function () {
-            this.triggerMethod("filterwith", this.model);
-        }
-    });
-    
-    var FiltersView = Marionette.CollectionView.extend({
-        className: "ui-grid-b ui-responsive",
-        childView: FilterButton
-    });
-   
     var DataForm = Backform.Form.extend({
         fields: [
             {
@@ -255,14 +114,6 @@ define([
         ]
     });
     
-    var Mode = Backbone.Model.extend({
-        
-    });
-    
-    var Modes = Backbone.Collection.extend({
-        model: Mode
-    })
-    
     var View = Marionette.LayoutView.extend({
         el: "#administration-descriptions > div[data-role='main']", 
         regions: {
@@ -319,7 +170,6 @@ define([
         setup: function() {
             var self = this;
             var options = self.options || {};
-            var modes = new Modes;
             self.filterOptions = new Backbone.Model({
                 playable: true,
                 category: "attributes",
