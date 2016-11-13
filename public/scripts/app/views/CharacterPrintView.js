85e62f43e2c4d869ad9f129d7bf28d50707e9555
Andrew Sayman <andrew.sayman@gmail.com>
2016-11-13 17:39:40 +0000

    Make print headers more dynamic

--- public/scripts/app/views/CharacterPrintView.js
+++ public/scripts/app/views/CharacterPrintView.js
@@ -31,59 +31,6 @@ define([
     section_html
 ) {
 
-    // Extends Backbone.View
-    var View = Backbone.View.extend( {
-
-        // The View Constructor
-        initialize: function() {
-            var self = this;
-            self.base_abilities = [];
-            self.base_abilities.push(["Academics", "Animal Ken", "Athletics", "Awareness", "Brawl", "Computer", "Crafts", "Dodge", "Drive"]);
-            self.base_abilities.push(["Empathy", "Firearms", "Intimidation", "Investigation", "Leadership", "Linguistics", "Lore", "Medicine", "Melee", "Occult"]);
-            self.base_abilities.push(["Performance", "Science", "Security", "Stealth", "Streetwise", "Subterfuge", "Survival"]);
-            self.base_abilities = _.flatten(self.base_abilities);
-            
-            _.bindAll(this,
-                "format_simpletext",
-                "format_attribute_value",
-                "format_attribute_focus",
-                "format_skill",
-                "format_specializations"
-            );
-        },
-
-        // Renders all of the Category models on the UI
-        render: function() {
-
-            var character = this.character;
-            var sortedSkills = character.get_sorted_skills();
-            var groupedSkills = character.get_grouped_skills(sortedSkills, 3);
-
-            // Sets the view's template property
-            this.template = _.template(character_print_view_html)(
-                {
-                    "character": this.character,
-                    "skills": sortedSkills,
-                    "groupedSkills": groupedSkills,
-                    format_simpletext: this.format_simpletext,
-                    format_attribute_value: this.format_attribute_value,
-                    format_attribute_focus: this.format_attribute_focus,
-                    format_skill: this.format_skill,
-                    format_specializations: this.format_specializations,
-                } );
-
-            // Renders the view's template inside of the current listview element
-            this.$el.find("div[role='main']").html(this.template);
-
-            // Maintains chainability
-            return this;
-
-        }
-
-    } );
-
-    _.extend(View.prototype, VampirePrintHelper);
-    
     var HeaderView = Marionette.ItemView.extend({
         template: _.template('<h1 class="ui-bar ui-bar-a"><%= format_simpletext("name") %></h1>'),
         templateHelpers: function() {
@@ -109,9 +56,9 @@ define([
         template: function(serialized_model) {
             var self = this;
             var tmpl = _.map(self.fields, function(field, i) {
-                if (serialized_model[field]) {
+                if (serialized_model[field.name]) {
                     return '<div class="ui-block-' + String.fromCharCode(97 + i) + '">\
-                                <h2 class="ui-bar ui-bar-a">' + _.capitalize(field) + ': <%= format_simpletext("' + field + '") %></h2>\
+                                <h2 class="ui-bar ui-bar-a">' + field.display + ': <%= format_simpletext("' + field.name + '") %></h2>\
                             </div>'
                 }
             })
@@ -127,11 +74,10 @@ define([
         },
         initialize: function(options) {
             var self = this;
-            this.fields = options.fields || ["clan", "archetype", "antecedence"]
+            this.fields = options.fields
             
             _.each(this.fields, function (field) {
-                self.listenTo(self.model, "change:" + field, self.render);
-                self.listenTo(self.model, "change:" + field, function() { console.log(field + " changed")});
+                self.listenTo(self.model, "change:" + field.name, self.render);
             });
             
             _.bindAll(this,
@@ -177,13 +123,11 @@ define([
         },
         initialize: function(options) {
             var self = this;
-            this.fields = options.fields || ["clan", "archetype", "antecedence"];
             this.$el.addClass("ui-grid-b");
             this.$el.addClass("ui-responsive");
             
             _.each(this.fields, function (field) {
                 self.listenTo(self.model, "change:" + field, self.render);
-                self.listenTo(self.model, "change:" + field, function() { console.log(field + " changed")});
             });
             
             _.bindAll(this,
@@ -223,7 +167,6 @@ define([
             );
         },
         onRender: function () {
-            console.log('onRender');
             this.$el = this.$el.children();
             this.$el.unwrap();
             this.setElement(this.$el);
@@ -305,7 +248,6 @@ define([
             );
         },
         onRender: function () {
-            console.log('onRender');
             this.$el = this.$el.children();
             this.$el.unwrap();
             this.setElement(this.$el);
@@ -379,7 +321,6 @@ define([
             );
         },
         onRender: function () {
-            console.log('onRender');
             this.$el = this.$el.children();
             this.$el.unwrap();
             this.setElement(this.$el);
@@ -420,11 +361,29 @@ define([
             self.showChildView('header', new HeaderView({model: character}), options);
             self.showChildView('firstbar', new TextBarView({
                 model: character,
-                fields: ["clan", "archetype", "antecedence"]
+                fields: [{
+                    name: "clan",
+                    display: "Clan"
+                },{
+                    name: "archetype",
+                    display: "Archetype", 
+                },{
+                    name:"antecedence",
+                    display: "Antecedence"
+                }]
             }), options);
             self.showChildView('secondbar', new TextBarView({
                 model: character,
-                fields: ["sect", "faction", "title"]
+                fields: [{
+                    name: "sect",
+                    display: "Sect"
+                },{
+                    name: "faction",
+                    display: "Faction", 
+                },{
+                    name:"title",
+                    display: "Title"
+                }]
             }), options);
             self.showChildView('attributes', new AttributesView({model: character}), options);
             self.showChildView('blood', new BloodView({
