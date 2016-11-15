6cef27fb8a0b6eccfd04e6ad061716fdc51bbb5f
Andrew Sayman <andrew.sayman@gmail.com>
2016-11-15 22:58:40 +0000

    Add new sample troupe id to the test configuration

--- public/scripts/app/testsiteconfig.js
+++ public/scripts/app/testsiteconfig.js
@@ -5,7 +5,8 @@ define([
     var ConfigPubstorm = {
         serverURL: "https://stagingapi.undergroundtheater.org/parse",
         facebookAppId: "1606746326305984",
-        redirect_uri: "https://stagingpatron.undergroundtheater.org/index.html"
+        redirect_uri: "https://stagingpatron.undergroundtheater.org/index.html",
+        SAMPLE_TROUPE_ID: "WOad4CBTsG"
     };
     
     var ConfigGnuLorienDev = ConfigPubstorm;
