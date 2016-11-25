2b1a7f7c8e36410ebb840ef9e5f8b413185e22d6
Andrew Sayman <andrew.sayman@gmail.com>
2016-11-25 17:42:17 +0000

    Remove transform description when printing

--- public/scripts/app/routers/mobileRouter.js
+++ public/scripts/app/routers/mobileRouter.js
@@ -432,6 +432,7 @@ define([
             $.mobile.loading("show");
             self.set_back_button("#character?" + cid);
             self.get_character(cid, "all").done(function (character) {
+                character.transform_description = [];
                 self.characterPrintView.setup(character);
                 $.mobile.changePage("#printable-sheet", {reverse: false, changeHash: false});
             }).fail(PromiseFailReport);
