28f2a77364700763ab1fed88f63e7f3f2a437933
Andrew Sayman <andrew.sayman@gmail.com>
2018-07-10 02:50:54 +0000

    Correct dist files

--- dist/scripts/app/siteconfig.js
+++ dist/scripts/app/siteconfig.js
@@ -1,1 +1,1 @@
-define([],function(){var r={serverURL:"https://stagingapi.undergroundtheater.org/parse",facebookAppId:"1606746326305984",redirect_uri:"https://stagingpatron.undergroundtheater.org/index.html"};return r});
+define([],function(){var r={serverURL:"https://api.undergroundtheater.org/parse",facebookAppId:"1606746326305984",redirect_uri:"https://patron.undergroundtheater.org"};return r});
