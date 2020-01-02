3a3d991150f094816a2da0df6372aef66d09b0ff
Andrew Sayman <andrew.sayman@gmail.com>
2020-01-02 18:15:53 -0500

    Transitioning from roles to troupes

--- public/scripts/app/views/PlayerOptionsView.js
+++ public/scripts/app/views/PlayerOptionsView.js
@@ -5,27 +5,25 @@ define([
     "text!../templates/player_options.html",
     "../helpers/PromiseFailReport",
     "parse",
-    "../helpers/RoleWreqr"
-], function( Backbone, Marionette, player_options_html, PromiseFailReport, Parse, RoleHelper) {
+    "../helpers/RoleWreqr",
+    "../helpers/TroupeWreqr",
+    "../collections/Troupes"
+], function( Backbone, Marionette, player_options_html, PromiseFailReport, Parse, RoleHelper, TroupeHelper, Troupes) {
 
-    var NoRolesView = Marionette.ItemView.extend({
+    var NoView = Marionette.ItemView.extend({
         template: _.template("Loading Your Troupes...")
     });
 
-    var RoleView = Marionette.ItemView.extend({
+    var TroupeView = Marionette.ItemView.extend({
         template: function (serialized_model) {
-            var id = serialized_model.attributes.name;
-            id = id.split('_');
-            id = id[1];
-            serialized_model.troupe_id = id;
-            return _.template("<a class='ui-btn' href='#troupe/<%= troupe_id %>/characters/all'><%= attributes.name %></a>")(serialized_model);
+            return _.template("<a class='ui-btn' href='#troupe/<%= objectId %>/characters/all'><%= name %></a>")(serialized_model);
         }
     });
 
-    var RolesView = Marionette.CollectionView.extend({
+    var TroupesView = Marionette.CollectionView.extend({
         tagName: 'div',
-        childView: RoleView,
-        emptyView: NoRolesView
+        childView: TroupeView,
+        emptyView: NoView
     });
 
     var View = Marionette.LayoutView.extend( {
@@ -37,7 +35,26 @@ define([
             var self = this;
             self.options = options || {};
             self.roles = RoleHelper.channel.reqres.request("all");
-            _.bindAll(this, "setup");
+            self.listenTo(self.roles, "add reset remove change", self.update_troupes);
+            self.troupes = new Troupes;
+            var global_troupes = TroupeHelper.channel.reqres.request("all");
+            self.listenTo(global_troupes, "add reset remove change", self.update_troupes);
+            _.bindAll(this, "setup", "update_troupes");
+        },
+        update_troupes: function() {
+            var self = this;
+            self._updateTroupeWrapper = self._updateTroupeWrapper || Parse.Promise.as();
+            self._updateTroupeWrapper = self._updateTroupeWrapper.always(function () {
+                var troupes = [];
+                _.each(self.roles.models, function(role) {
+                    var id = role.attributes.attributes.name;
+                    id = id.split('_');
+                    id = id[1];
+                    troupes.push(TroupeHelper.channel.reqres.request("get", id));
+                });
+                self.troupes.reset(troupes);
+            });
+            return self._updateTroupeWrapper;
         },
         setup: function() {
             var self = this;
@@ -51,10 +68,12 @@ define([
             if (!is_ad && !is_st)
                 return self;
 
-            self.showChildView('troupcharactersquickaccess', new RolesView({
-                collection: self.roles
+            self.showChildView('troupcharactersquickaccess', new TroupesView({
+                collection: self.troupes
             }), options);
-            RoleHelper.get_current_roles().fail(PromiseFailReport);
+            // Get the troupes first so we have them when the roles start
+            TroupeHelper.get_troupes().fail(PromiseFailReport);
+            RoleHelper.get_current_roles().fail(PromiseFailReport);;
 
             return self;
         },
