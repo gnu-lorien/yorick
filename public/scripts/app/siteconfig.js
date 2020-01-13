9b82b466ac37a0f2272bdb0b33068f0627586f6d
Andrew Sayman <andrew.sayman@gmail.com>
2020-01-12 19:04:14 -0500

    Default site reference to heroku app

--- public/scripts/app/siteconfig.js
+++ public/scripts/app/siteconfig.js
@@ -1,19 +1,6 @@
 // Includes file dependencies
 define([
 ], function () {
-
-    var ConfigPubstorm = {
-        serverURL: "https://stagingapi.undergroundtheater.org/parse",
-        facebookAppId: "1606746326305984",
-        redirect_uri: "https://stagingpatron.undergroundtheater.org/index.html"
-    };
-
-    var ConfigPatron = {
-        serverURL: "https://api.undergroundtheater.org/parse",
-        facebookAppId: "1606746326305984",
-        redirect_uri: "https://patron.undergroundtheater.org"
-    };
-
     var ConfigLocalhost = {
         serverURL: "http://localhost:1337/parse",
         facebookAppId: "1607159299598020",
@@ -27,21 +14,21 @@ define([
         SAMPLE_TROUPE_ID: "k7zf9B7bwV"
     };
 
-    var ConfigC9 = {
-        serverURL: "https://yorick-latest-parse-server-gnu-lorien.c9users.io/parse",
-        redirect_uri: "https://yorick-latest-parse-server-gnu-lorien.c9users.io/index.html",
-        SAMPLE_TROUPE_ID: "mXhRByDNxX"
-    };
-
     var ConfigHeroku = {
         serverURL: "https://young-plateau-55863.herokuapp.com/parse",
         facebookAppId: "202279720650237",
         redirect_uri: "https://sheets.ourislandgeorgia.net/index.html",
         SAMPLE_TROUPE_ID: "mXhRByDNxX"
     };
-    
-    var ConfigGnuLorienDev = ConfigC9;
+
+    var ConfigAfterTwilight = {
+        serverURL: "https://after-twilight-yorick.herokuapp.com/parse",
+        facebookAppId: "202279720650237",
+        redirect_uri: "https://sheets.ourislandgeorgia.net/index.html",
+        SAMPLE_TROUPE_ID: "mXhRByDNxX"
+    };
+
+    var ConfigGnuLorienDev = ConfigAfterTwilight;
     
     return ConfigGnuLorienDev;
-
 });
