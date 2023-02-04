24904a187f25f0a6180b285ae74832ad74667ae2
Andrew Sayman <andrew.sayman@gmail.com>
2023-02-04 18:01:00 -0500

    Test connecting to my local parse

--- test/basic.test.ts
+++ test/basic.test.ts
@@ -1,7 +1,20 @@
 import { describe, expect, it } from 'vitest'
+import { Parse } from 'parse/node'
 
 describe('tests', () => {
   it('should works', () => {
     expect(1 + 1).toEqual(2)
+  })
+})
+
+describe('parse sanity', () => {
+  it('Initialize and use Parse', async () => {
+    Parse.initialize('APPLICATION_ID')
+    Parse.serverURL = 'http://127.0.0.1:1337/parse/1'
+    const Description = Parse.Object.extend('Description')
+    const query = new Parse.Query(Description)
+    query.equalTo('name', 'Orthodox')
+    const results = await query.find()
+    expect(results[0].get('name')).toBe('Orthodox')
   })
 })
