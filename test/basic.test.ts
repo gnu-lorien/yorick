313282298b0157183f89f2dd2c088c0f8325ebe1
Andrew Sayman <andrew.sayman@gmail.com>
2023-02-11 18:07:32 -0500

    Import more of the old tests

--- test/basic.test.ts
+++ test/basic.test.ts
@@ -1,3 +1,4 @@
+import * as _ from 'lodash-es'
 import { beforeAll, beforeEach, describe, expect, it } from 'vitest'
 // import { Parse } from 'parse/node'
 import Parse from 'parse/dist/parse.min.js'
@@ -6,6 +7,7 @@ import { useConfigTestDefault } from '~/composables/siteconfig'
 import { SampleVampire } from '~/models/SampleVampire'
 import { Vampire } from '~/models/Vampire'
 import { registerYorickTypes } from '~/modules/parsetypes'
+import { useCharacterStore } from '~/stores/characters'
 
 describe('tests', () => {
   it('should works', () => {
@@ -25,19 +27,28 @@ describe('parse sanity', () => {
   })
 })
 
+async function parseInit() {
+  Parse.initialize('APPLICATION_ID')
+  Parse.serverURL = useConfigTestDefault().serverURL
+  registerYorickTypes()
+}
+async function parseStart() {
+  await parseInit()
+  if (Parse.User.current() === null || !_.eq(Parse.User.current().get('username'), 'devuser'))
+    return await Parse.User.logIn('devuser', 'thedumbness')
+
+  return Parse.User.current()
+}
+
+async function parseEnd() {
+  if (Parse.User.current())
+    Parse.User.logOut()
+}
+
 describe('Vampires', () => {
   beforeAll(async () => {
-    Parse.initialize('APPLICATION_ID')
-    Parse.serverURL = useConfigTestDefault().serverURL
-    if (Parse.User.current())
-      Parse.User.logOut()
-
-    await Parse.User.logIn('devuser', 'thedumbness')
-    registerYorickTypes()
-    return async () => {
-      if (Parse.User.current())
-        Parse.User.logOut()
-    }
+    await parseStart()
+    return parseEnd
   })
   beforeEach(() => {
     setActivePinia(createPinia())
@@ -49,5 +60,233 @@ describe('Vampires', () => {
   it('Create a regular vampire', async () => {
     const v = await Vampire.create_test_character('saymsamp')
     expect(v.get('name')).to.be.a('string').and.satisfy(msg => msg.startsWith('kct_saymsamp'))
+  })
+})
+
+/*
+const character_types = [
+  {
+    name: "Vampire",
+    template: Vampire
+  },{
+    name: "Werewolf",
+    template: Werewolf
+  }
+];
+*/
+
+const character_types = [
+  {
+    name: 'Vampire',
+    template: Vampire,
+  },
+]
+
+_.each(character_types, (character_type) => {
+  describe(`A ${character_type.name}'s traits`, () => {
+    let vampire
+    let expected_change_length
+
+    beforeAll(async () => {
+      await parseStart()
+      const v = await character_type.template.create_test_character('vampiretraits')
+      expected_change_length = 5
+      const characters = useCharacterStore()
+      vampire = await characters.getCharacter(v.id, character_type.template)
+    })
+
+    it('show up in the history', async () => {
+      let changes = await vampire.get_recorded_changes()
+      expect(changes.models.length).toBe(expected_change_length)
+      await vampire.update_trait('Haven', 1, 'backgrounds', 0, true)
+      expected_change_length++
+      changes = await vampire.get_recorded_changes()
+      expect(changes.models.length).toBe(expected_change_length)
+      await vampire.update_trait('Haven', 1, 'backgrounds', 0, true)
+      changes = await vampire.get_recorded_changes()
+      expect(changes.models.length).toBe(expected_change_length)
+      await vampire.update_trait('Haven', 2, 'backgrounds', 0, true)
+      expected_change_length++
+      changes = await vampire.get_recorded_changes()
+      expect(changes.models.length).toBe(expected_change_length)
+    })
+
+    it('can be renamed', (done) => {
+      const start_check = expected_change_length
+      let first_trait_id
+      let second_trait_id
+      let third_trait_id
+      vampire.update_trait('Retainers', 1, 'backgrounds', 0, true).done((trait) => {
+        expected_change_length++
+        trait.set('name', 'Retainers: Specialized Now')
+        return vampire.update_trait(trait)
+      }).done((trait) => {
+        expected_change_length++
+        first_trait_id = trait.id
+        trait.set('name', 'Retainers: Specialized Again')
+        trait.set('value', 4)
+        return vampire.update_trait(trait)
+      }).done((trait) => {
+        expected_change_length++
+        trait.set('value', 5)
+        return vampire.update_trait(trait)
+      }).done(() => {
+        expected_change_length++
+        return vampire.update_trait('Retainers: Specialized Now', 2, 'backgrounds', 0)
+      }).done((trait) => {
+        second_trait_id = trait.id
+        expected_change_length++
+        return vampire.update_trait('Retainers', 3, 'backgrounds', 0)
+      }).done((trait) => {
+        third_trait_id = trait.id
+        expected_change_length++
+        return vampire.update_trait('Retainers: Specialized Now', 4, 'backgrounds', 0)
+      }).done(() => {
+        expected_change_length++
+        return vampire.update_trait('Retainers', 4, 'backgrounds', 0, true)
+      }).done(() => {
+        expected_change_length++
+        return vampire.get_recorded_changes()
+      }).done((changes) => {
+        expect(changes.models.length).toBe(expected_change_length)
+        _(changes.models).slice(start_check, changes.length).each((change, i) => {
+          expect(change.get('name')).not.toBe(undefined)
+          const name = change.get('name')
+          const startsWithRetainers = _.startsWith(name, 'Retainers')
+          if (startsWithRetainers) {
+            if (i == 0) {
+              expect(change.get('type')).toBe('define')
+              expect(change.get('value')).toBe(1)
+              expect(change.get('cost')).toBe(1)
+              expect(change.get('simple_trait_id')).toBe(first_trait_id)
+            }
+            else if (i == 1) {
+              expect(change.get('type')).toBe('update')
+              expect(change.get('value')).toBe(1)
+              expect(change.get('cost')).toBe(1)
+              expect(change.get('name')).toBe('Retainers: Specialized Now')
+              expect(change.get('old_text')).toBe('Retainers')
+              expect(change.get('simple_trait_id')).toBe(first_trait_id)
+            }
+            else if (i == 2) {
+              expect(change.get('type')).toBe('update')
+              expect(change.get('value')).toBe(4)
+              expect(change.get('cost')).toBe(10)
+              expect(change.get('name')).toBe('Retainers: Specialized Again')
+              expect(change.get('old_text')).toBe('Retainers: Specialized Now')
+              expect(change.get('simple_trait_id')).toBe(first_trait_id)
+            }
+            else if (i == 3) {
+              expect(change.get('type')).toBe('update')
+              expect(change.get('old_value')).toBe(4)
+              expect(change.get('value')).toBe(5)
+              expect(change.get('old_cost')).toBe(10)
+              expect(change.get('cost')).toBe(15)
+              expect(change.get('name')).toBe('Retainers: Specialized Again')
+              expect(change.get('old_text')).toBe('Retainers: Specialized Again')
+              expect(change.get('simple_trait_id')).toBe(first_trait_id)
+            }
+            else if (i == 4) {
+              expect(change.get('type')).toBe('define')
+              expect(change.get('value')).toBe(2)
+              expect(change.get('cost')).toBe(3)
+              expect(change.get('name')).toBe('Retainers: Specialized Now')
+              expect(change.get('simple_trait_id')).toBe(second_trait_id)
+            }
+            else if (i == 5) {
+              expect(change.get('type')).toBe('define')
+              expect(change.get('value')).toBe(3)
+              expect(change.get('cost')).toBe(6)
+              expect(change.get('name')).toBe('Retainers')
+              expect(change.get('simple_trait_id')).toBe(third_trait_id)
+            }
+            else if (i == 6) {
+              expect(change.get('type')).toBe('update')
+              expect(change.get('value')).toBe(4)
+              expect(change.get('old_value')).toBe(2)
+              expect(change.get('old_cost')).toBe(3)
+              expect(change.get('cost')).toBe(10)
+              expect(change.get('name')).toBe('Retainers: Specialized Now')
+              expect(change.get('old_text')).toBe('Retainers: Specialized Now')
+              expect(change.get('simple_trait_id')).toBe(second_trait_id)
+            }
+            else if (i == 7) {
+              expect(change.get('type')).toBe('update')
+              expect(change.get('value')).toBe(4)
+              expect(change.get('old_value')).toBe(3)
+              expect(change.get('old_cost')).toBe(6)
+              expect(change.get('cost')).toBe(10)
+              expect(change.get('name')).toBe('Retainers')
+              expect(change.get('old_text')).toBe('Retainers')
+              expect(change.get('simple_trait_id')).toBe(third_trait_id)
+            }
+          }
+        })
+      }).done(() => {
+        done()
+      }).fail((error) => {
+        done.fail(error)
+      })
+    })
+
+    it('can\'t be renamed to collide', (done) => {
+      let classic_trait, not_classic_trait
+      vampire.update_trait('Retainers: Classic', 1, 'backgrounds', 0, true).done((trait) => {
+        classic_trait = trait
+        return vampire.update_trait('Retainers: Not Classic', 2, 'backgrounds', 0, true)
+      }).done((trait) => {
+        not_classic_trait = trait
+        not_classic_trait.set('name', 'Retainers: Classic')
+        return vampire.update_trait(not_classic_trait)
+      }).done(() => {
+        done.fail('Allowed the rename to be persisted')
+      }).fail((error) => {
+        expect(error.code).toBe(1)
+        expect(not_classic_trait.get('name')).toBe('Retainers: Not Classic')
+        done()
+      })
+    })
+
+    it('can fail to be removed', (done) => {
+      // Change the prototype of simpletrait to make destroy fail
+      const old_destroy = SimpleTrait.prototype.destroy
+      SimpleTrait.prototype.destroy = function () {
+        // var e = new Parse.Error(Parse.Error.INVALID_LINKED_SESSION, "Forcing SimpleTrait destroy to fail");
+        return Parse.Promise.error({})
+      }
+
+      // Remove the thing
+      vampire.get_trait_by_name('backgrounds', 'Haven').then((st) => {
+        return vampire.remove_trait(st).then(() => {
+          SimpleTrait.prototype.destroy = old_destroy
+          done.fail('Successfully removed a trait while destroy was broken')
+        }, (error) => {
+          SimpleTrait.prototype.destroy = old_destroy
+          // Make sure we didn't remove the thing
+          vampire.get_trait_by_name('backgrounds', 'Haven').then((fa) => {
+            expect(fa.get('value')).toBe(2)
+            expect(fa.get('free_value')).toBe(0)
+            done()
+          }, (error) => {
+            done.fail(error)
+          })
+        })
+      })
+    })
+
+    it('can be removed', (done) => {
+      vampire.get_trait_by_name('backgrounds', 'Haven').then((st) => {
+        expect(st).toBeDefined()
+        expect(st.id).toBeDefined()
+        return vampire.remove_trait(st)
+      }).then(() => {
+        return vampire.get_trait_by_name('backgrounds', 'Haven')
+      }).then((fa) => {
+        expect(fa).toBeUndefined()
+        done()
+      }).fail((error) => {
+        done.fail(`Failed to remove the trait ${JSON.stringify(error)}`)
+      })
+    })
   })
 })
