9736198eee61374d4c7b991892827c63fa1599c7
Andrew Sayman <andrew.sayman@gmail.com>
2023-02-19 18:57:17 -0500

    First pass with a working list view of characters
    Using https://getbootstrap.com/docs/5.3/components/list-group/#custom-content to build out something that looks a bit like the old card

--- src/pages/characters/[type].vue
+++ src/pages/characters/[type].vue
@@ -10,7 +10,7 @@ const characters = ref([])
 
 onMounted(async () => {
   const characterStore = useCharacterStore()
-  const newCharacters = await characterStore.getUserCharacters(Parse.User.current())
+  const newCharacters = await characterStore.getUserCharacters(Parse.User.current(), props.type, 5)
   characters.value.splice(0, newCharacters.length, ...newCharacters)
 })
 
@@ -20,8 +20,24 @@ defineExpose([name])
 
 <template>
   all characters
+  <div class="list-group">
+    <a v-for="character in characters" href="#" class="list-group-item list-group-item-action active">
+      <div class="d-flex w-100 justify-content-between">
+        <img :src="character.get_thumbnail_sync(128)">
+        <h2>{{ character.get('name') }}</h2>
+        <template v-if="character.get('type') === 'Werewolf'">
+          <template v-if="character.get('wta_tribe') || character.get('wta_breed') || character.get('wta_auspice')">
+            <p>{{ character.get("wta_tribe") }} {{ character.get("wta_breed") }} {{ character.get("wta_auspice") }}</p>
+          </template>
+        </template>
+        <p>{{ character.get("wta_faction") }} {{ character.get("archetype") }} {{ character.get("wta_camp") }}</p>
+        <p>{{ character.get("antecedence") }}</p>
+      </div>
+    </a>
+  </div>
   <ol>
     <li v-for="character in characters">
+      <img :src="character.get_thumbnail_sync(128)">
       {{ character }}
     </li>
   </ol>
