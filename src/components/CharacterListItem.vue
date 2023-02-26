f914b85b1180e21890d594bea650e07f48114d41
Andrew Sayman <andrew.sayman@gmail.com>
2023-02-26 16:00:09 -0500

    Style looks closer to the original
    It's still blue for some reason that I'll have to look into later

--- src/components/CharacterListItem.vue
+++ src/components/CharacterListItem.vue
@@ -7,34 +7,40 @@ const character = await characters.getCharacter(props.characterId)
 </script>
 
 <template>
-  <div class="d-flex w-100 justify-content-between">
-    <img :src="character.get_thumbnail_sync(128)">
-    <h2>{{ character.get('name') }}</h2>
-    <template v-if="character.get('type') === 'Werewolf'">
-      Tis a werewolf!
-      <template v-if="character.get('wta_tribe') || character.get('wta_breed') || character.get('wta_auspice')">
-        <p>{{ character.get("wta_tribe") }} {{ character.get("wta_breed") }} {{ character.get("wta_auspice") }}</p>
-      </template>
-      <p>{{ character.get("wta_faction") }} {{ character.get("archetype") }} {{ character.get("wta_camp") }}</p>
-      <p>{{ character.get("antecedence") }}</p>
+  <img :src="character.get_thumbnail_sync(128)" class="rounded float-start">
+  <h5 class="mb-1 text-start ps-5">
+    {{ character.get('name') }}
+  </h5>
+  <template v-if="character.get('type') === 'Werewolf'">
+    <template v-if="character.get('wta_tribe') || character.get('wta_breed') || character.get('wta_auspice')">
+      <p>{{ character.get("wta_tribe") }} {{ character.get("wta_breed") }} {{ character.get("wta_auspice") }}</p>
     </template>
-    <template v-else-if="character.get('type') === 'ChangelingBetaSlice'">
-      Changeling's not supported yet
+    <p>{{ character.get("wta_faction") }} {{ character.get("archetype") }} {{ character.get("wta_camp") }}</p>
+    <p>{{ character.get("antecedence") }}</p>
+  </template>
+  <template v-else-if="character.get('type') === 'ChangelingBetaSlice'">
+    Changeling's not supported yet
+  </template>
+  <template v-else>
+    <p class="text-start ps-5">
+      {{ character.get("sect") }} {{ character.get("archetype") }} {{ character.get("clan") }}
+    </p>
+    <template v-if="character.get('antecedence') || character.get('faction') || character.get('title')">
+      <p class="text-start ps-5">
+        {{ character.get("antecedence") }} {{ character.get("faction") }} {{ character.get("title") }}
+      </p>
     </template>
-    <template v-else>
-      Tis a vampire!y
-      <p>{{ character.get("sect") }} {{ character.get("archetype") }} {{ character.get("clan") }}</p>
-      <template v-if="character.get('antecedence') || character.get('faction') || character.get('title')">
-        <p>{{ character.get("antecedence") }} {{ character.get("faction") }} {{ character.get("title") }}</p>
-      </template>
-    </template>
-    <template v-if="character.has('owner') && character.get('owner').get('username')">
-      <p>{{ character.get('owner').get('realname') }} {{ character.get('owner').get('email') }} {{ character.get('owner').get('username') }}</p>
-    </template>
-    <template v-else>
-      <p>DELETED</p>
-    </template>
-  </div>
+  </template>
+  <template v-if="character.has('owner') && character.get('owner').get('username')">
+    <p class="text-start ps-5">
+      {{ character.get('owner').get('realname') }} {{ character.get('owner').get('email') }} {{ character.get('owner').get('username') }}
+    </p>
+  </template>
+  <template v-else>
+    <p class="text-start ps-5">
+      DELETED
+    </p>
+  </template>
 </template>
 
 <style scoped>
