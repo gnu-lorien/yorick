69bce55fa8c408a05e054a7ccad6672bf77e283e
Andrew Sayman <andrew.sayman@gmail.com>
2023-02-26 14:44:29 -0500

    Use the character list item component

--- src/pages/characters/[type].vue
+++ src/pages/characters/[type].vue
@@ -7,11 +7,13 @@ const props = defineProps({
 })
 
 const characters = ref([])
+const loading = ref(true)
 
 onMounted(async () => {
   const characterStore = useCharacterStore()
   const newCharacters = await characterStore.getUserCharacters(Parse.User.current(), props.type, 5)
   characters.value.splice(0, newCharacters.length, ...newCharacters)
+  loading.value = false
 })
 
 const name = '[type]'
@@ -20,27 +22,27 @@ defineExpose([name])
 
 <template>
   all characters
-  <div class="list-group">
-    <a v-for="character in characters" href="#" class="list-group-item list-group-item-action active">
-      <div class="d-flex w-100 justify-content-between">
-        <img :src="character.get_thumbnail_sync(128)">
-        <h2>{{ character.get('name') }}</h2>
-        <template v-if="character.get('type') === 'Werewolf'">
-          <template v-if="character.get('wta_tribe') || character.get('wta_breed') || character.get('wta_auspice')">
-            <p>{{ character.get("wta_tribe") }} {{ character.get("wta_breed") }} {{ character.get("wta_auspice") }}</p>
+  <template v-if="loading">
+    Loading all characters
+  </template>
+  <template v-else>
+    <div class="list-group">
+      <a v-for="character in characters" :key="character.id" href="#" class="list-group-item list-group-item-action active">
+        <Suspense>
+          <template #fallback>
+            <div>Loading {{ character.id }}</div>
           </template>
-        </template>
-        <p>{{ character.get("wta_faction") }} {{ character.get("archetype") }} {{ character.get("wta_camp") }}</p>
-        <p>{{ character.get("antecedence") }}</p>
-      </div>
-    </a>
-  </div>
-  <ol>
-    <li v-for="character in characters">
-      <img :src="character.get_thumbnail_sync(128)">
-      {{ character }}
-    </li>
-  </ol>
+          <CharacterListItem :character-id="character.id" />
+        </Suspense>
+      </a>
+    </div>
+    <ol>
+      <li v-for="character in characters">
+        <img :src="character.get_thumbnail_sync(128)">
+        {{ character }}
+      </li>
+    </ol>
+  </template>
 </template>
 
 <style scoped>
