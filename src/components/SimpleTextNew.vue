843dd1a6010008f85c973feb188d0d373904865a
Andrew Sayman <andrew.sayman@gmail.com>
2023-03-11 12:50:05 -0500

    Basic filtering

--- src/components/SimpleTextNew.vue
+++ src/components/SimpleTextNew.vue
@@ -11,6 +11,7 @@ const character = await characters.getCharacter(props.characterId)
 
 const descriptionStore = useDescriptionStore()
 const descriptions = await descriptionStore.getDescriptionsForCategory(props.category)
+const filter = ref('')
 
 const router = useRouter()
 
@@ -19,11 +20,25 @@ async function selectDescription(description) {
   triggerRef(character)
   router.push(props.redirectTo)
 }
+
+const filteredDescriptions = computed(() => {
+  if (filter.value.trim() === '')
+    return [...descriptions]
+  const filtered = []
+  for (const description of descriptions) {
+    if (description.get('name').toLowerCase().startsWith(filter.value.toLowerCase()))
+      filtered.push(description)
+  }
+  return filtered
+})
 </script>
 
 <template>
   <div class="list-group">
-    <button v-for="description in descriptions" class="list-group-item list-group-item-action text-start" @click="selectDescription(description)">
+    <form class="p-2 mb-2 bg-light border-bottom">
+      <input v-model="filter" type="search" class="form-control" autocomplete="false" placeholder="Type to filter...">
+    </form>
+    <button v-for="description in filteredDescriptions" class="list-group-item list-group-item-action text-start" @click="selectDescription(description)">
       {{ description.get("name") }}
     </button>
   </div>
