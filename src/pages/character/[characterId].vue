2d38d148d8f374b9cdda0735fc79e52a9c9f29bf
Andrew Sayman <andrew.sayman@gmail.com>
2023-03-10 19:05:46 -0500

    Stand in for the main character page

---
+++ src/pages/character/[characterId].vue
@@ -0,0 +1,19 @@
+<script setup>
+import { useCharacterStore } from '~/stores/characters'
+const props = defineProps(['characterId'])
+
+const name = '[characterId]'
+defineExpose([name])
+
+const characters = useCharacterStore()
+const character = await characters.getCharacter(props.characterId)
+</script>
+
+<template>
+  your current character
+  {{ character.id }}
+</template>
+
+<style scoped>
+
+</style>
