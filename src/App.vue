e079f591e7a13844a403dadbbb4b12667913c485
Andrew Sayman <andrew.sayman@gmail.com>
2023-03-10 19:05:33 -0500

    Set up the router view to be able to handle async setups

--- src/App.vue
+++ src/App.vue
@@ -22,5 +22,21 @@ useHead({
 </script>
 
 <template>
-  <RouterView />
+  <RouterView v-slot="{ Component }">
+    <template v-if="Component">
+      <Transition mode="out-in">
+        <KeepAlive>
+          <Suspense>
+            <!-- main content -->
+            <component :is="Component" />
+
+            <!-- loading state -->
+            <template #fallback>
+              Loading...
+            </template>
+          </Suspense>
+        </KeepAlive>
+      </Transition>
+    </template>
+  </RouterView>
 </template>
