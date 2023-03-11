0ef1081083b1dcf58dd26469b3ae84b7154f7f8c
Andrew Sayman <andrew.sayman@gmail.com>
2023-03-10 21:56:04 -0500

    Add the descriptions to the simple text picking page

--- src/components/SimpleTextNew.vue
+++ src/components/SimpleTextNew.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { useCharacterStore } from '~/stores/characters'
+import { useDescriptionStore } from '~/stores/descriptions'
 const props = defineProps(['category', 'target', 'characterId', 'redirectTo'])
 
 const name = '[new]'
@@ -7,11 +8,15 @@ defineExpose([name])
 
 const characters = useCharacterStore()
 const character = await characters.getCharacter(props.characterId)
+
+const descriptionStore = useDescriptionStore()
+const descriptions = await descriptionStore.getDescriptionsForCategory(props.category)
 </script>
 
 <template>
   {{ props.redirectTo }}
   {{ character }}
+  {{ descriptions }}
 </template>
 
 <style scoped>
