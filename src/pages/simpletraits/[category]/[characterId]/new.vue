f59f05ca80fe4f056dc28afa6767ce46cb209df7
Andrew Sayman <andrew.sayman@gmail.com>
2023-03-12 14:55:23 -0400

    Create new from fauxtrait
    May have hit a bug with vue when updating the specialized text

--- src/pages/simpletraits/[category]/[characterId]/new.vue
+++ src/pages/simpletraits/[category]/[characterId]/new.vue
@@ -1,5 +1,6 @@
 <script setup>
 import * as _ from 'lodash-es'
+import { FauxTrait } from '~/helpers/FauxTrait'
 import { useCharacterStore } from '~/stores/characters'
 const props = defineProps(['category', 'characterId'])
 
@@ -9,6 +10,9 @@ const details = character.value.simpletrait_details(props.category)
 const router = useRouter()
 const route = useRoute()
 
+const picking = ref(true)
+const fauxtrait = ref({})
+
 function redirectOnSelected(trait) {
   const redirectPath = _.get(route, 'query.redirectPath', '')
   if (redirectPath) {
@@ -17,20 +21,39 @@ function redirectOnSelected(trait) {
   }
   router.push({ name: 'simpletrait-category-characterId-simpleTraitId', params: { ...props, simpleTraitId: trait.id } })
 }
+
+function switchToEditing(trait) {
+  fauxtrait.value = trait
+  picking.value = false
+}
 </script>
 
 <template>
-  <Suspense>
+  <Suspense v-if="picking">
     <template #fallback>
       Loading...
     </template>
     <SimpleTraitPick
       :category="props.category"
       :character-id="props.characterId"
-      @selected.once="(trait) => redirectOnSelected(trait)"
+      :faux="true"
+      @selected.once="(trait) => switchToEditing(trait)"
     >
       New trait for {{ details.pretty }}
     </SimpleTraitPick>
+  </Suspense>
+  <Suspense v-else>
+    <template #fallback>
+      Loading...
+    </template>
+    <SimpleTraitView
+      :category="props.category"
+      :character-id="props.characterId"
+      :fauxtrait="fauxtrait"
+      @selected.once="(trait) => redirectOnSelected(trait)"
+    >
+      New trait for {{ details.pretty }}
+    </SimpleTraitView>
   </Suspense>
 </template>
 
